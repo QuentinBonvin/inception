f6e85d20af44f142b1a4d91498a93e168ac7dc99
Quentin <bonvin.quentin@gmail.com>
2023-09-25 13:14:01 +0200

    update repo

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,15 +1,30 @@
+# service mysql start;
+
+# sleep 5
+
+# mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+# mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
+# mysql -e "FLUSH PRIVILEGES;"
+
+# mysqladmin -u root -p "${SQL_ROOT_PASSWORD}" shutdown
+
+# exec mysqld_safe
+
+
 service mysql start;
+sleep 5
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-
-mysql -e "FLUSH PRIVILEGES;"
-
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysql -u root -p"${SQL_ROOT_PWD}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DB}\`;"
+mysql -u root -p"${SQL_ROOT_PWD}" -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PWD}';"
+mysql -u root -p"${SQL_ROOT_PWD}" -e "GRANT ALL PRIVILEGES ON \`${SQL_DB}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PWD}';"
+mysql -u root -p"${SQL_ROOT_PWD}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PWD}';"
+mysql -u root -p"${SQL_ROOT_PWD}" -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p"${SQL_ROOT_PWD}" shutdown
 
 exec mysqld_safe
--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,7 +1,28 @@
+#!/bin/bash
+
 sleep 10
 
-wp config create	--allow-root \
-	--dbname=$SQL_DATABASE \
-	--dbuser=$SQL_USER \
-	--dbpass=$SQL_PASSWORD \
-	--dbhost=mariadb:3306 --path='/var/www/wordpress'
+mkdir -p /run/php
+
+# wp config create	--allow-root \
+# 	--dbname=$SQL_DATABASE \
+# 	--dbuser=$SQL_USER \
+# 	--dbpass=$SQL_PASSWORD \
+# 	--dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+cd /var/www/wordpress/
+
+wp core install --allow-root \
+    --url=${WP_URL} \
+    --title=${WP_TITLE} \
+    --admin_user=${WP_ADMIN} \
+    --admin_email=${WP_EMAIL} \
+    --admin_password=${WP_ADMIN_PWD}
+
+wp user create --allow-root\
+    ${WP_DB_USER}\
+    ${WP_EMAIL_USER} \
+    --role=author\
+    --user_pass=${WP_DB_PWD}
+
+/usr/sbin/php-fpm7.3 --nodaemonize
